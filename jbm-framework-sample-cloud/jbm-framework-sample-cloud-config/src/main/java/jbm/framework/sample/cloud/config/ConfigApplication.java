35efbf02aadddd5a49fd210184071bdd9c77d64e
numen06 <numen06@sina.com>
2018-12-16 01:39:29 +0800

    测试添加

---
+++ jbm-framework-sample-cloud/jbm-framework-sample-cloud-config/src/main/java/jbm/framework/sample/cloud/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package jbm.framework.sample.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: create by wesley
+ * @date:2018/12/15
+ */
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
--- jbm-framework-sample-mysql/src/main/java/com/jbm/sample/mysql/MySqlAppStart.java
+++ jbm-framework-sample-mysql/src/main/java/com/jbm/sample/mysql/MySqlAppStart.java
@@ -2,12 +2,15 @@ package com.jbm.sample.mysql;
 
 import com.jbm.sample.mysql.entity.AggregateData;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.jbm.micro.boot.MicroBootApplication;
-
-@MicroBootApplication
+@SpringBootApplication
+@EnableTransactionManagement
 @EntityScan(basePackageClasses = AggregateData.class)
+@ComponentScan("com.jbm.sample")
 public class MySqlAppStart {
     public static void main(String[] args) {
         SpringApplication.run(MySqlAppStart.class, args);
