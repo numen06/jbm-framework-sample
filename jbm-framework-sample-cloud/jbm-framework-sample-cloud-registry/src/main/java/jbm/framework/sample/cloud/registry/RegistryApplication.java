534e8ac89a2ae89b933a861efc910148108b48a2
numen06 <numen06@sina.com>
2018-12-16 01:59:33 +0800

    测试添加

---
+++ jbm-framework-sample-cloud/jbm-framework-sample-cloud-registry/src/main/java/jbm/framework/sample/cloud/registry/RegistryApplication.java
@@ -0,0 +1,15 @@
+package jbm.framework.sample.cloud.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: create by wesley
+ * @date:2018/12/15
+ */
+@SpringBootApplication
+public class RegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
